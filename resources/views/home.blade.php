9f087860d2ebec4026b497872c63f7c330a45fd8
majaul1989 <majadul1989@gmail.com>
2017-08-26 15:31:28 +0600

    fix same code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,29 @@
 
 @section('content')
 
-<div class="container-fluid">
+<div class="container-fluid ">
     <div class="row">
         <div class="col-md-12 com-sm-12 col-lg-12 col-xs-12">
-        <div class="row">
-        div.col-md-3  col-lg-3       
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                        <div class="canvasMenuLeft">
+                        <ul>
+                            <li>Dashboard</li>
+                            <li>Profile</li>
+                            <li>Settings</li>
+                            <li>Logout</li>
+                        </ul>   
+                        </div><!-- canvasLeft -->
+                    </div>
+                    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+                        sidebar
+                    </div>
                 </div>
+                    
+                </div>       
             </div>
-        </div>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::group(['middleware'=>'auth'],function(){
 
 Auth::routes();
 
-Route::get('/dashboard', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
